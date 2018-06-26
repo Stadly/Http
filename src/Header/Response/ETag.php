1238c4086d4cade5ef318afa8e23505b959a8cc5
Stadly <magnar@myrtveit.com>
2018-06-26 11:26:16 +0200

    ETag

---
+++ src/Header/Response/ETag.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\Http\Header\Response;
+
+use Stadly\Http\Header\Value\EntityTag;
+
+/**
+ * Class for handling the HTTP header field ETag.
+ *
+ * Specification: https://tools.ietf.org/html/rfc7232#section-2.3
+ */
+final class ETag implements HeaderInterface
+{
+    /**
+     * @var EntityTag Entity tag.
+     */
+    private $entityTag;
+
+    /**
+     * Constructor.
+     *
+     * @param EntityTag $entityTag Entity tag.
+     */
+    public function __construct(EntityTag $entityTag)
+    {
+        $this->setEntityTag($entityTag);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString(): string
+    {
+        return $this->getName().': '.$this->getValue();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return 'ETag';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getValue(): string
+    {
+        return (string)$this->entityTag;
+    }
+
+    /**
+     * @return EntityTag Entity tag.
+     */
+    public function getEntityTag(): EntityTag
+    {
+        return $this->entityTag;
+    }
+
+    /**
+     * Set entity tag.
+     *
+     * @param EntityTag $entityTag Entity tag.
+     */
+    public function setEntityTag(EntityTag $entityTag): void
+    {
+        $this->entityTag = $entityTag;
+    }
+}
---
+++ tests/Header/Response/ETagTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\Http\Header\Response;
+
+use PHPUnit\Framework\TestCase;
+use Stadly\Http\Header\Value\EntityTag;
+
+/**
+ * @coversDefaultClass \Stadly\Http\Header\Response\ETag
+ * @covers ::<protected>
+ * @covers ::<private>
+ */
+final class ETagTest extends TestCase
+{
+    /**
+     * @covers ::__construct
+     */
+    public function testCanConstructETag(): void
+    {
+        $eTag = new ETag(new EntityTag('foo'));
+        
+        // Force generation of code coverage
+        $eTagConstruct = new ETag(new EntityTag('foo'));
+        self::assertEquals($eTag, $eTagConstruct);
+    }
+
+    /**
+     * @covers ::__toString
+     */
+    public function testCanConvertETagToString(): void
+    {
+        $eTag = new ETag(new EntityTag('foo'));
+
+        self::assertSame('ETag: "foo"', (string)$eTag);
+    }
+
+    /**
+     * @covers ::__toString
+     */
+    public function testCanConvertWeakETagToString(): void
+    {
+        $eTag = new ETag(new EntityTag('foo', /*isWeak*/true));
+
+        self::assertSame('ETag: W/"foo"', (string)$eTag);
+    }
+
+    /**
+     * @covers ::getName
+     */
+    public function testCanGetName(): void
+    {
+        $eTag = new ETag(new EntityTag('foo'));
+
+        self::assertSame('ETag', $eTag->getName());
+    }
+
+    /**
+     * @covers ::getValue
+     */
+    public function testCanGetValueForETag(): void
+    {
+        $eTag = new ETag(new EntityTag('foo'));
+
+        self::assertSame('"foo"', $eTag->getValue());
+    }
+
+    /**
+     * @covers ::getValue
+     */
+    public function testCanGetValueForWeakETag(): void
+    {
+        $eTag = new ETag(new EntityTag('foo', /*isWeak*/true));
+
+        self::assertSame('W/"foo"', $eTag->getValue());
+    }
+
+    /**
+     * @covers ::getEntityTag
+     */
+    public function testCanGetEntityTag(): void
+    {
+        $entityTag = new EntityTag('foo');
+        $eTag = new ETag($entityTag);
+        
+        self::assertSame($entityTag, $eTag->getEntityTag());
+    }
+
+    /**
+     * @covers ::setEntityTag
+     */
+    public function testCanSetEntityTag(): void
+    {
+        $entityTag = new EntityTag('bar');
+        $eTag = new ETag($entityTag);
+        
+        $eTagSetType = new ETag(new EntityTag('foo'));
+        $eTagSetType->setEntityTag($entityTag);
+
+        self::assertEquals($eTag, $eTagSetType);
+    }
+}
